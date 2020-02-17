40495eb02cc5a5464bba6d89bbc36aa0c062b61d
Ayush Kumar Singh <45496026+Ayush-KS@users.noreply.github.com>
2020-02-17 23:40:19 +0530

    Second Commit

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-console.log("Script loaded!")
+$('#click-one').click(function(){
+    $('#click-one').hide(500);
+    $('#click-two').show(1000, "swing");
+
+    $('#click-two').click(function() {
+        $('#click-two').hide(500);
+        $('#image').show(1000);
+        $('#click-one').text("name your first born boy: ayushi");
+        $('#click-one').css({'width': '300px', 'cursor' : 'default'});
+        $('#click-one').off();
+        $('#click-one').show(3000);
+
+    })
+})
